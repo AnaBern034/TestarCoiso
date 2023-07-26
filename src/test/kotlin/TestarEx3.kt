fd7b0be84e443be05f5975f0203d402e3ff96fa7
ana <ana.bernardes@zup.com.br>
2023-07-26 11:28:09 -0300

    primeiro

---
+++ src/test/kotlin/TestarEx3.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TestarEx3 {
+
+    @Test
+    fun TestarMaiorNumero(){
+        val numero = listOf(10,2,3,6)
+        val maiorNumero = numero.max()
+
+        assertEquals(10,maiorNumero)
+    }
+}
